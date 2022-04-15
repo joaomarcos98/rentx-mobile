f6d797229a811bdfffaeda161c8ee63f25d3de15
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-04-15 14:44:11 -0300

    initial: first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import * as Styled from './styles';
+
+export const Home = () => {
+    return (
+        <Styled.Container>
+            <Styled.Title>
+                HOME
+            </Styled.Title>
+
+        </Styled.Container>
+    );
+}
