30d52047e3305b354683e34ecd4540806065b2a5
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-04-18 14:51:19 -0300

    add: create a theme for app

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,5 +4,9 @@ export const Container = styled.View`
     flex: 1;
     align-items: center;
     justify-content: center;
+    
+    background-color: ${({ theme }) => theme.colors.main};
 `
-export const Title = styled.Text``
+export const Title = styled.Text`
+    font-size: 30px;
+`
