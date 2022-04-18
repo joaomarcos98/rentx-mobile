8a1d6ec55b51bb0ec577e715ea180b6a850db4db
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-04-18 15:25:39 -0300

    add: create header for home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import { StatusBar } from "react-native";
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from "../../assets/logo.svg";
 
 import * as Styled from './styles';
 
 export const Home = () => {
     return (
         <Styled.Container>
-            <Styled.Title>
-                HOME
-            </Styled.Title>
+            <StatusBar
+                barStyle='light-content'
+                backgroundColor="transparent"
+                translucent
+            />
+            <Styled.Header>
+                <Styled.HeaderContent>
+                    <Logo
+                        height={RFValue(12)}
+                        width={RFValue(108)}
+                    />
+                    <Styled.TotalCars>
+                        Total de 12 Carros
+                    </Styled.TotalCars>
+                </Styled.HeaderContent>
+            </Styled.Header>
 
         </Styled.Container>
     );
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,12 +1,35 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
     flex: 1;
     align-items: center;
-    justify-content: center;
     
-    background-color: ${({ theme }) => theme.colors.main};
+    background-color: ${({ theme }) => theme.colors.background_primary};
 `
-export const Title = styled.Text`
-    font-size: 30px;
+export const Header = styled.View`
+    width: 100%;
+    height: 113px;
+
+    justify-content: flex-end;
+    padding: 32px 16px;
+
+
+    background-color: ${({ theme }) => theme.colors.header};
+`
+
+export const HeaderContent = styled.View`
+
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`
+
+
+export const TotalCars = styled.Text`
+    font-size: ${RFValue(15)}px;
+
+    font-family: ${({ theme }) => theme.fonts.primary_400};
+    color: ${({ theme }) => theme.colors.text};
+    
 `
