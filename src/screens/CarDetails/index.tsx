aa9f42fdc97d739b5daf2719052b3a5555275539
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-04-19 08:51:52 -0300

    add: create details for screen Car Details

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -20,6 +20,29 @@ export const CarDetails = () => {
                     ]}
                 />
             </Styled.CarImages>
+
+            <Styled.Content>
+                <Styled.Details>
+
+                    <Styled.Description>
+                        <Styled.Brand>Audi</Styled.Brand>
+                        <Styled.Name>Rs 5</Styled.Name>
+                    </Styled.Description>
+
+                    <Styled.Rent>
+                        <Styled.Period>Ao dia</Styled.Period>
+                        <Styled.Price>R$ 500</Styled.Price>
+                    </Styled.Rent>
+
+                </Styled.Details>
+
+                    <Styled.About>
+                        Carrin bacana do meccheferson doidera maravilhosa opa fion n sei oq mas ta otimo revoada do tubarão
+                    </Styled.About>
+
+
+            </Styled.Content>
+
         </Styled.Container>
     );
 }
