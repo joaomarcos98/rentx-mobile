7f090ab61d0978dddd0e9ef8d26c62eeef96f2f5
João Marcos <joomarcos.belanga98@gmail.com>
2022-05-13 15:23:44 -0300

    modify: upgrade the image slider

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { FlatList, ViewToken } from 'react-native';
 
 import * as Styled from './styles';
 
@@ -7,22 +8,52 @@ type ImageSliderProps = {
     imagesUrl: string[];
 }
 
+type ChangeImageProps = {
+    viewableItems: ViewToken[];
+    changed: ViewToken[];
+}
+
 export const ImageSlider = ({ imagesUrl }: ImageSliderProps) => {
+
+    const [imageIndex, setImageIndex] = useState(0);
+
+    const indexChanged = useRef((info: ChangeImageProps) => {
+        const index = info.viewableItems[0].index!;
+
+        setImageIndex(index)
+    })
+
     return (
         <Styled.Container>
             <Styled.ImageIndexes>
-                <Styled.ImageIndex active />
-                <Styled.ImageIndex active={false} />
-                <Styled.ImageIndex active={false} />
-                <Styled.ImageIndex active={false} />
+                {
+                    imagesUrl.map((_, index) => (
+                        <Styled.ImageIndex
+                            key={index}
+                            active={index === imageIndex}
+                        />
+                    ))
+                }
+
             </Styled.ImageIndexes>
 
-            <Styled.Content>
-                <Styled.CarImage
-                    source={{ uri: imagesUrl[0] }}
-                    resizeMode="contain"
-                />
-            </Styled.Content>
+
+            <FlatList
+                data={imagesUrl}
+                keyExtractor={key => key}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                onViewableItemsChanged={indexChanged.current}
+                renderItem={({ item }) => (
+                    <Styled.Content>
+                        <Styled.CarImage
+                            source={{ uri: item }}
+                            resizeMode="contain"
+                        />
+                    </Styled.Content>
+                )}
+            />
+
         </Styled.Container>
     );
 } 
